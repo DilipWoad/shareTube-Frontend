c460261b49b55a053bd3efc4ff53f0a85ad54416
Dilip Woad <dilipwoad777@gmail.com>
2026-01-14 13:47:24 +0530

    hide .env and use proxy to use youtube search api

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,7 +2,7 @@ const YOUTUBE_VIDEOS_URL =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
   import.meta.env.VITE_YOUTUBE_API_KEY;
 
-const YOUTUBE_SEARCH_URL = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+const YOUTUBE_SEARCH_URL = "https://thingproxy.freeboard.io/fetch/http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 const OFFSET_LIVE_CHAT = 10;
 
